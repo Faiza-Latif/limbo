8e086b1698cc95012dfd79e2da3d16a17555bb8e
faiza <faiza@outlook.pt>
2020-10-25 11:33:09 +0000

    Added login reactive form with validators.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-filled: boolean = false;
-  constructor() { }
+    filled: boolean = false;
+    loginForm = this.fb.group({
+        email: [null, Validators.required],
+        password: [null, Validators.required],
+    });
 
-  ngOnInit() {
+    constructor(private fb: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.onChanges();
+    }
+
+    login() {
+        console.log(this.loginForm.valid);
+    }
+
+  onChanges(): void {
+    this.loginForm.valueChanges.subscribe(data => {
+      this.filled = this.loginForm.valid;
+    });
   }
-
 }
